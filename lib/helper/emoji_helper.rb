bedfec3c39871f70cf3df635fb4d85bcf83fe5e7
ID25 <xid25x@gmail.com>
2016-01-31 18:41:59 +0300

    Add emoji helper

--- lib/helper/emoji_helper.rb
+++ lib/helper/emoji_helper.rb
@@ -1,5 +1,37 @@
+require_relative './emoji_regex'
+
 module RailsEmojiPicker
-  def show_me
-    '<p>Hello</p>'.html_safe
+  def emojify(content, css_class = 'emoji')
+    h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+      if emoji = Emoji.find_by_alias(Regexp.last_match(1))
+        %(<img alt="#{Regexp.last_match(1)}" src="#{image_path("emoji/#{emoji.image_filename}")}" class="#{css_class}"/>)
+      else
+        match
+      end
+    end.html_safe if content.present?
+  end
+
+  def content_with_emoji(body)
+    post = find_emoji(body)
+    emojify(post, 'emoji-show')
+  end
+
+  private
+
+  def find_emoji(text)
+    chars = text.split('')
+    chars.each do |c|
+      emoji = c.scan(regex).join('')
+      c.gsub!(regex, emoji_name(Emoji.find_by_unicode(emoji).name)) if emoji.present?
+    end
+    chars.join('')
+  end
+
+  def emoji_name(emoji)
+    ":#{emoji}:"
+  end
+
+  def regex
+    RailsEmojiPicker::EmojiRegex.regex
   end
 end
--- lib/rails_emoji_picker.rb
+++ lib/rails_emoji_picker.rb
@@ -1,5 +1,6 @@
 require 'rails_emoji_picker/version'
 require 'rails_emoji_picker/engine'
+require 'gemoji'
 require 'helper/emoji_helper'
 require 'rails_emoji_picker/railtie'
 
