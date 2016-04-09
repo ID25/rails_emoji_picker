a3031a239610ba6eae82f4925e402e6d38d610a7
ID25 <xid25x@gmail.com>
2016-04-09 18:19:17 +0300

    Add test for host and path

--- spec/emoji_helper_spec.rb
+++ spec/emoji_helper_spec.rb
@@ -6,8 +6,18 @@ describe RailsEmojiPicker do
   describe '#content_with_emoji' do
     correct_emoji_format = helper.content_with_emoji('Test Kitty emoji 🐱')
 
-    it 'return correct emoji name' do
-      expect(correct_emoji_format).to eq "Test Kitty emoji <span class='emoji-image'><img alt='cat' class=\"emoji\" src=\"http://localhost:3000/cat.png\"></span>"
+    it 'return correct image src with emoji' do
+      expect(correct_emoji_format).to eq "Test Kitty emoji <span class='emoji-image'><img alt='cat' class=\"emoji\" src=\"#{Emoji.asset_host}#{Emoji.asset_path}/cat.png\"></span>"
+    end
+  end
+
+  describe 'custom asset_host and asset_path' do
+    it 'return img src tag with custom host and path' do
+      Emoji.asset_host = 'https://mysite.com/'
+      Emoji.asset_path = '/images/emoji/'
+      string = helper.content_with_emoji('Test String 🐱')
+
+      expect(string).to eq "Test String <span class='emoji-image'><img alt='cat' class=\"emoji\" src=\"https://mysite.com/images/emoji//cat.png\"></span>"
     end
   end
 end
--- spec/rails_emoji_picker_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe RailsEmojiPicker do
-  it 'has a version number' do
-    expect(RailsEmojiPicker::VERSION).not_to be nil
-  end
-end
