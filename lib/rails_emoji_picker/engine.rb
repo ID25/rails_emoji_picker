7c7e47fb016b11b5b5a598393a9628da6e71dd09
Volodymyr.Partyka <volodymyr.partyka@teamvoy.com>
2017-04-19 17:08:40 +0300

    update rails_emoji_picker/engine

--- lib/rails_emoji_picker/engine.rb
+++ lib/rails_emoji_picker/engine.rb
@@ -1,6 +1,6 @@
 module RailsEmojiPicker
   class Engine < Rails::Engine
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     initializer 'initialize rails_emoji_picker helper' do |_app|
       ActiveSupport.on_load(:action_view) do
