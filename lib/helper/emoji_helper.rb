9c6de562f4f0790e03956f10ec64e95fddefbb53
Jaime Arroyo Navia <jaime.arroyo.navia@gmail.com>
2017-09-27 16:24:28 -0300

    only on production make the especial convertion on other environment make the stanadrt_replace

--- lib/helper/emoji_helper.rb
+++ lib/helper/emoji_helper.rb
@@ -62,11 +62,12 @@ module RailsEmojiPicker
   def insert_image_to_image_tag(string, img)
     return stanadrt_replace(string, img) unless defined? Rails
 
-    if Rails.env.development?
-      stanadrt_replace(string, img)
-    elsif Rails.env.production?
+    
+    if Rails.env.production?
       url = image_tag("emoji/#{img[:name]}.png")[/img.*?src="(.*?)"/i, 1]
       string.gsub!(img[:char], url)
+    else
+      stanadrt_replace(string, img)
     end
   end
 
